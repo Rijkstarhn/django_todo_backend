2a555c20cbc9b9c22631ba1ed52f04aaa7734fac
Rijkstarhn <841206675@qq.com>
2025-01-20 16:31:51 -0800

    Added pagination.

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -49,8 +49,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,  # Number of items per page
 }
 
 MIDDLEWARE = [
