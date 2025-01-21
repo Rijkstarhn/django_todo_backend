ddb1c14355d32f4c8f7eb2a3f8122b5ce2a347b9
Rijkstarhn <841206675@qq.com>
2025-01-20 16:15:01 -0800

    Added another api involving RetrieveUpdateDestroyAPIView.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListAPI
+from .views import TaskListAPI, TaskDetailAPI
 
 urlpatterns = [
     path('tasks/', TaskListAPI.as_view(), name='task_list_api'),
+    path('tasks/<int:pk>/', TaskDetailAPI.as_view(), name='task_detail_api'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,6 @@ from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
-
 # Create your views here.
 class TaskListAPI(APIView):
     def get(self, request):
@@ -19,3 +18,7 @@ class TaskListAPI(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class TaskDetailAPI(RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
