ee3e81a136ce014668d32c741aeb9d6741f18cff
Rijkstarhn <841206675@qq.com>
2025-01-20 16:30:59 -0800

    Added authentication to apis.

--- tasks/views.py
+++ tasks/views.py
@@ -5,8 +5,12 @@ from .models import Task
 from .serializers import TaskSerializer
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 class TaskListAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
@@ -20,5 +24,7 @@ class TaskListAPI(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class TaskDetailAPI(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -39,7 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tasks',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,  # Number of items per page
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
